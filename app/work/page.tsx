93d2e4149e720fa781f36955b809afb385526c7f
elohughes-arch <elohughes@icloud.com>
2026-05-13 14:15:22 +0100

    Delete app directory

--- app/work/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function WorkPage() {
-  return (
-    <div className="min-h-screen bg-wegrow-dark text-white pt-32 px-6 flex flex-col items-center">
-      <h1 className="text-5xl font-serif mb-6">Our Work</h1>
-      <p className="text-wegrow-lime text-xl">Explore our past contracting projects.</p>
-    </div>
-  );
-}
